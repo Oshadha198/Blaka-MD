7e7e588c3790ba69d028d17e07fa7b223be39535
Oshadha198 <oshadhakarunathilaka586@gmail.com>
2025-08-23 21:06:15 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/Oshadha198/blaka_photo/refs/heads/main/Untitled.png",
               },
               caption: sid,
             });
@@ -129,5 +129,6 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
 
 
